dd1c0f36ca688951b614090c9b3f02745ccfe714
Taras <taras190789@gmail.com>
2021-04-18 17:10:26 +0300

    version 1

--- src/main/java/com/kaf22/codezilla/RPiBot.java
+++ src/main/java/com/kaf22/codezilla/RPiBot.java
@@ -65,7 +65,7 @@ public class RPiBot extends TelegramLongPollingBot {
 
             execute(message);
         } else if (inputText.equals("Information")) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("vcgenccmd measure_temp").getInputStream()));
+            BufferedReader br = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("vcgencmd measure_temp").getInputStream()));
 
             String line;
 
